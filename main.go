aed9c105455868182a99f226938e307414233d48
hariirawan <hari31121993@gmail.com>
2024-01-31 14:42:02 +0800

    setup project, create endpoint users & login

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bwastartup/handler"
+	"bwastartup/user"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "root:ztoko123@tcp(103.175.217.64:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
+
+	userHandler := handler.NewUserHandler(userService)
+
+	router := gin.Default()
+	api := router.Group("/api/v1")
+
+	api.POST("/users", userHandler.RegisterUser)
+	api.POST("/login", userHandler.Login)
+
+	router.Run()
+}
